1c16cc7ec91cb4610051070407a1aee60390340a
Rai Lima <raioliveira.ec@gmail.com>
2025-05-22 00:29:11 -0300

    Remove arquivos desnecessários

--- api/AwsBackupTracker/Models/BackupLog.cs
+++
@@ -1,9 +0,0 @@
-namespace BackupDashboard.Models;
-
-public class BackupLog
-{
-    public string Timestamp { get; set; } = "";
-    public string Status { get; set; } = "";
-    public string Duration { get; set; } = "";
-    public string Output { get; set; } = "";
-}
